7f66d133ebfb62076a596da5ac1c40cebaac1564
NohaCode <gammagx@gmail.com>
2024-04-29 23:06:27 +0900

    #7.3 Movie App part One
    [노마드 코드 강의]
    강의 : ReactJS로 영화 웹 서비스 만들기
    목표
    - 영화 정보를 알려주는 API 사용해서 받아온 데이터로 화면에 display (part 1)
    - useState, useEffect, map 사용

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import ToDoApp from "./ToDo";
 import CoinTrackerApp from "./CoinTrackerApp";
+import MovieApp from "./MovieApp";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(<ToDoApp />);
-root.render(<CoinTrackerApp />);
+root.render(<MovieApp />);
