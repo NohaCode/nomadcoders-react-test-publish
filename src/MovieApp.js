ff5568f8904f8ed1bda6777defde5f6f9a1e38d0
NohaCode <gammagx@gmail.com>
2024-05-01 16:29:19 +0900

    #7.4 Movie App part Two
    [노마드 코드 강의]
    강의 : ReactJS로 영화 웹 서비스 만들기
    목표 : 지난 시간 코드 중 Movie 부분을 component화

--- src/MovieApp.js
+++ src/MovieApp.js
@@ -1,52 +1,7 @@
-import { useEffect, useState } from "react";
+import Home from "./routes/Home";
 
 function MovieApp() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    // ## inline variables ##
-    // const json = await (
-    //   await fetch(
-    //     "https:/yts.mx/api/v2/list_movies.json?minimun_rating=8.5?sort_by=year"
-    //   )
-    // ).json();
-
-    const response = await fetch(
-      "https:/yts.mx/api/v2/list_movies.json?minimun_rating=8.8?sort_by=year"
-    );
-    const json = await response.json();
-
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  console.log(movies);
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} />
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres && movie.genres.map((g) => <li key={g}>{g}</li>)}
-                {/* https://tlsdnjs12.tistory.com/56 
-                -> movie.genres가 아직 로딩이 되지 않았을 때 오류가 날 수 있음. 
-                그래서 movie.genres가 로딩이 될 때 map을 실행해야 함. 
-                안 그러면 undefined에 map을 쓸 수 없다는 오류가 뜸 */}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return <Home />;
 }
 
 export default MovieApp;
---
+++ src/components/Movie.js
@@ -0,0 +1,25 @@
+//import PropTypes form "prop-types";
+
+function Movie({ coverImg, title, summary, genres }) {
+  return (
+    <div>
+      <img src={coverImg} alt={title} />
+      <h2>{title}</h2>
+      <p>{summary}</p>
+      <ul>{genres && genres.map((g) => <li key={g}>{g}</li>)}</ul>
+      {/* https://tlsdnjs12.tistory.com/56 
+        -> movie.genres가 아직 로딩이 되지 않았을 때 오류가 날 수 있음. 
+        그래서 movie.genres가 로딩이 될 때 map을 실행해야 함. 
+        안 그러면 undefined에 map을 쓸 수 없다는 오류가 뜸 */}
+    </div>
+  );
+}
+
+// Movie.propTypes = {
+//     coverImg: PropTypes.string.isRequred,
+//     title: PropTypes.string.isRequred,
+//     summary: PropTypes.string.isRequred,
+//     genres: PropTypes.arrayOf(PropTypes.string).isRequred,
+// };
+
+export default Movie;
